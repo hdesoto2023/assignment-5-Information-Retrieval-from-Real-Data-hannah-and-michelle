7f28547936f376c453492f15249e1c5beb67bad5
Hannah DeSoto <hannahdesotom@gmail.com>
2023-12-08 20:12:04 -0800

    added flat clusters to src

---
+++ src/FlatClusters.py
@@ -0,0 +1,94 @@
+energyBurned.tail(10)
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+class HeartRateAnalyzer:
+    def __init__(self, file_path):
+        self.df = pd.read_csv(file_path)
+        self.features = None
+        self.features_standardized = None
+        self.kmeans = None
+        self.model = None
+
+    def preprocess_data(self):
+        # convert timestamps to datetime objects
+        self.df['creationDate'] = pd.to_datetime(self.df['creationDate'])
+        self.df['startDate'] = pd.to_datetime(self.df['startDate'])
+        self.df['endDate'] = pd.to_datetime(self.df['endDate'])
+
+        self.df['duration'] = (self.df['endDate'] - self.df['startDate']).dt.total_seconds() / 60
+        self.df['time_of_day'] = self.df['startDate'].dt.hour + self.df['startDate'].dt.minute / 60
+
+    def cluster_data(self, num_clusters=3):
+        self.features = self.df[['value', 'duration', 'time_of_day']]
+        scaler = StandardScaler()
+        self.features_standardized = scaler.fit_transform(self.features)
+
+        self.kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+        self.df['cluster'] = self.kmeans.fit_predict(self.features_standardized)
+
+    def visualize_clusters(self):
+        plt.scatter(self.df['startDate'], self.df['value'], c=self.df['cluster'], cmap='viridis')
+        plt.xlabel('Date')
+        plt.ylabel('Resting Heart Rate (count/min)')
+        plt.title('Resting Heart Rate Clusters Over Time')
+        plt.show()
+
+    def train_regression_model(self):
+        self.df['days_since_start'] = (self.df['startDate'] - self.df['startDate'].min()).dt.days
+        X_train, X_test, y_train, y_test = train_test_split(
+            self.df[['days_since_start', 'cluster']], self.df['value'], test_size=0.2, random_state=42
+        )
+        self.model = LinearRegression()
+        self.model.fit(X_train, y_train)
+
+        y_pred = self.model.predict(X_test)
+
+        mse = mean_squared_error(y_test, y_pred)
+        print(f'Mean Squared Error: {mse}')
+
+    def predict_future_heart_rate(self, num_years=20):
+        # Group data by month and calculate the average resting heart rate
+        monthly_avg_heart_rate = self.df.groupby(self.df['startDate'].dt.to_period("M"))['value'].mean().reset_index()
+
+        # Plot the average resting heart rate by month
+        plt.plot(monthly_avg_heart_rate['startDate'].dt.to_timestamp(), monthly_avg_heart_rate['value'], marker='o')
+        plt.xlabel('Month')
+        plt.ylabel('Average Resting Heart Rate (count/min)')
+        plt.title('Average Resting Heart Rate by Month')
+        plt.show()
+
+        # Predict resting heart rate for the next 20 years
+        days_future = pd.DataFrame({
+            'days_since_start': range(self.df['days_since_start'].max() + 1,
+                                      self.df['days_since_start'].max() + num_years * 365 + 1),
+            'cluster': self.kmeans.predict(self.features_standardized.mean(axis=0).reshape(1, -1))
+        })
+        future_predictions = self.model.predict(days_future[['days_since_start', 'cluster']])
+
+        # Visualize predictions
+        plt.plot(self.df['startDate'], self.df['value'], label='Actual Data', marker='o')
+        plt.plot(days_future['days_since_start'], future_predictions, label='Predictions', linestyle='--', marker='o')
+        plt.xlabel('Days Since Start')
+        plt.ylabel('Resting Heart Rate (count/min)')
+        plt.title('Resting Heart Rate Prediction for the Next 20 Years')
+        plt.legend()
+        plt.show()
+
+# Provide the file path to your CSV file
+file_path = '/Users/hannahdesoto/PycharmProjects/assignment-5-Information-Retrieval-from-Real-Data-hannah-and-michelle/data/RestingHeartRate.csv'
+# Create an instance of HeartRateAnalyzer
+analyzer = HeartRateAnalyzer(file_path)
+
+# Perform analysis
+analyzer.preprocess_data()
+analyzer.cluster_data()
+analyzer.visualize_clusters()
+analyzer.train_regression_model()
+analyzer.predict_future_heart_rate()
