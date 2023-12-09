fe4b225c273b901e7a946ffc4fe29771f2840b4d
Hannah DeSoto <hannahdesotom@gmail.com>
2023-12-09 08:35:42 -0800

    trying to print predicted values for heart rates

--- src/FlatClusters.py
+++ src/FlatClusters.py
@@ -56,6 +56,10 @@ class HeartRateAnalyzer:
         # Group data by month and calculate the average resting heart rate
         monthly_avg_heart_rate = self.df.groupby(self.df['startDate'].dt.to_period("M"))['value'].mean().reset_index()
 
+        # Print monthly averages to terminal
+        print("Monthly Heart Rate Averages:")
+        print(monthly_avg_heart_rate[['startDate', 'value']])
+
         # Plot the average resting heart rate by month
         plt.plot(monthly_avg_heart_rate['startDate'].dt.to_timestamp(), monthly_avg_heart_rate['value'], marker='o')
         plt.xlabel('Month')
@@ -71,58 +75,74 @@ class HeartRateAnalyzer:
         })
         future_predictions = self.model.predict(days_future[['days_since_start', 'cluster']])
 
+        # Group the future data by month and calculate the average resting heart rate
+        future_avg_heart_rate = days_future.groupby(
+            pd.to_datetime(days_future['days_since_start'], unit='D').dt.to_period("M")
+        )['cluster'].count().reset_index()
+
+        # Print predicted averages to terminal for each month of the next five years
+        print("\nPredicted Monthly Heart Rate Averages for the Next 5 Years:")
+        for idx, row in future_avg_heart_rate.iterrows():
+            month_year = row['days_since_start'].strftime('%B %Y')
+            predicted_value = future_predictions[idx]
+            print(f"{month_year}: {predicted_value}")
+
         # Visualize predictions
         plt.plot(self.df['startDate'], self.df['value'], label='Actual Data', marker='o')
-        plt.plot(days_future['days_since_start'], future_predictions, label='Predictions', linestyle='--', marker='o')
+        plt.plot(
+            pd.to_datetime(future_avg_heart_rate['days_since_start'], unit='D').dt.to_timestamp(),
+            future_predictions, label='Predictions', linestyle='--', marker='o'
+        )
         plt.xlabel('Days Since Start')
         plt.ylabel('Resting Heart Rate (count/min)')
         plt.title('Resting Heart Rate Prediction for the Next 20 Years')
         plt.legend()
         plt.show()
 
-class WorkoutDataClusterer:
-    def __init__(self, file_path):
-        self.df = pd.read_csv(file_path)
-        self.features = None
-        self.features_standardized = None
-        self.kmeans = None
 
-    def preprocess_data(self):
-        # convert timestamps to datetime objects
-        self.df['startDate'] = pd.to_datetime(self.df['startDate'])
-        self.df['endDate'] = pd.to_datetime(self.df['endDate'])
+# class WorkoutDataClusterer:
+#     def __init__(self, file_path):
+#         self.df = pd.read_csv(file_path)
+#         self.features = None
+#         self.features_standardized = None
+#         self.kmeans = None
+#
+#     def preprocess_data(self):
+#         # convert timestamps to datetime objects
+#         self.df['startDate'] = pd.to_datetime(self.df['startDate'])
+#         self.df['endDate'] = pd.to_datetime(self.df['endDate'])
+#
+#         # Extract month and year information
+#         self.df['month'] = self.df['startDate'].dt.month
+#         self.df['year'] = self.df['startDate'].dt.year
+#
+#         self.df['duration'] = (self.df['endDate'] - self.df['startDate']).dt.total_seconds() / 60
+#
+#     def cluster_data(self, num_clusters=3):
+#         self.features = self.df[['year', 'month', 'duration']]
+#         scaler = StandardScaler()
+#         self.features_standardized = scaler.fit_transform(self.features)
+#
+#         self.kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+#         self.df['cluster'] = self.kmeans.fit_predict(self.features_standardized)
+#
+#     def visualize_clusters(self):
+#         # Plot the clusters
+#         plt.scatter(self.df['startDate'], self.df['duration'], c=self.df['cluster'], cmap='viridis')
+#         plt.xlabel('Date')
+#         plt.ylabel('Workout Duration (min)')
+#         plt.title('Workout Duration Clusters Over Time')
+#         plt.show()
+#
+# # Provide the file path to your workout data CSV file
+# workout_file_path = '/Users/hannahdesoto/PycharmProjects/assignment-5-Information-Retrieval-from-Real-Data-hannah-and-michelle/data/Workout.csv'
+# # Create an instance of WorkoutDataClusterer
+# workout_clusterer = WorkoutDataClusterer(workout_file_path)
 
-        # Extract month and year information
-        self.df['month'] = self.df['startDate'].dt.month
-        self.df['year'] = self.df['startDate'].dt.year
-
-        self.df['duration'] = (self.df['endDate'] - self.df['startDate']).dt.total_seconds() / 60
-
-    def cluster_data(self, num_clusters=3):
-        self.features = self.df[['year', 'month', 'duration']]
-        scaler = StandardScaler()
-        self.features_standardized = scaler.fit_transform(self.features)
-
-        self.kmeans = KMeans(n_clusters=num_clusters, random_state=42)
-        self.df['cluster'] = self.kmeans.fit_predict(self.features_standardized)
-
-    def visualize_clusters(self):
-        # Plot the clusters
-        plt.scatter(self.df['startDate'], self.df['duration'], c=self.df['cluster'], cmap='viridis')
-        plt.xlabel('Date')
-        plt.ylabel('Workout Duration (min)')
-        plt.title('Workout Duration Clusters Over Time')
-        plt.show()
-
-# Provide the file path to your workout data CSV file
-workout_file_path = '/Users/hannahdesoto/PycharmProjects/assignment-5-Information-Retrieval-from-Real-Data-hannah-and-michelle/data/Workout.csv'
-# Create an instance of WorkoutDataClusterer
-workout_clusterer = WorkoutDataClusterer(workout_file_path)
-
-# Perform clustering
-workout_clusterer.preprocess_data()
-workout_clusterer.cluster_data()
-workout_clusterer.visualize_clusters()
+# # Perform clustering
+# workout_clusterer.preprocess_data()
+# workout_clusterer.cluster_data()
+# workout_clusterer.visualize_clusters()
 
 # Provide the file path to your CSV file
 file_path = '/Users/hannahdesoto/PycharmProjects/assignment-5-Information-Retrieval-from-Real-Data-hannah-and-michelle/data/RestingHeartRate.csv'
