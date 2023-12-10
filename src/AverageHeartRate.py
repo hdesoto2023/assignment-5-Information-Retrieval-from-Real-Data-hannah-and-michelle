8c0f22ef654643f8e62d4e9f289fe648482e04ac
Hannah DeSoto <hannahdesotom@gmail.com>
2023-12-10 13:46:57 -0800

    forecasting done by flat clustesr

--- src/AverageHeartRate.py
+++ src/AverageHeartRate.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.arima.model import ARIMA
+
 
 class RestingHeartRateAnalyzer:
 
@@ -25,15 +27,54 @@ class RestingHeartRateAnalyzer:
         if self.df is not None:
             monthly_average_hr = self.calculate_monthly_average()
             plt.figure(figsize=(10, 6))
-            plt.plot(monthly_average_hr['month_year'], monthly_average_hr['value'], marker='o', linestyle='-', color='b')
+            plt.plot(monthly_average_hr['month_year'], monthly_average_hr['value'], marker='o', linestyle='-',
+                     color='b')
             plt.title('Monthly Average Resting Heart Rate')
             plt.xlabel('Month')
             plt.ylabel('Average Resting Heart Rate')
             plt.grid(True)
+            plt.xticks(rotation=45)  # Rotate x-axis labels
             plt.show()
 
+    def forecast_next_24_months(self):
+        if self.df is not None:
+            monthly_average_hr = self.calculate_monthly_average()
+            monthly_average_hr['month_year'] = pd.to_datetime(monthly_average_hr['month_year'])
+
+            # Fit an ARIMA model
+            model = ARIMA(monthly_average_hr['value'], order=(1, 1, 1))
+            fit_model = model.fit()
+
+            # Forecast the next 24 months
+            forecast_values = fit_model.get_forecast(steps=24).predicted_mean
+
+            # Create a DataFrame for the forecast
+            forecast_df = pd.DataFrame({
+                'month_year': pd.date_range(monthly_average_hr['month_year'].max() + pd.DateOffset(months=1),
+                                            periods=24, freq='M'),
+                'value': forecast_values
+            })
+
+            # Plot the original data and the forecast
+            plt.figure(figsize=(10, 6))
+            plt.plot(monthly_average_hr['month_year'], monthly_average_hr['value'], marker='o', linestyle='-',
+                     color='b', label='Original Data')
+            plt.plot(forecast_df['month_year'], forecast_df['value'], marker='o', linestyle='--', color='r',
+                     label='Forecast')
+            plt.title('Monthly Average Resting Heart Rate Forecast')
+            plt.xlabel('Month')
+            plt.ylabel('Average Resting Heart Rate')
+            plt.grid(True)
+            plt.legend()
+            plt.xticks(rotation=45)
+            plt.show()
+
+
 if __name__ == "__main__":
-    analyzer = RestingHeartRateAnalyzer('/Users/michellejee/Desktop/assignment-5-Information-Retrieval-from-Real-Data-hannah-and-michelle/data/RestingHeartRate.csv')
+    analyzer = RestingHeartRateAnalyzer(
+        '/Users/hannahdesoto/PycharmProjects/assignment-5-Information-Retrieval-from-Real-Data-hannah-and-michelle/data/RestingHeartRate.csv')
     analyzer.load_data()
     analyzer.preprocess_data()
     analyzer.plot_monthly_average()
+    analyzer.forecast_next_24_months()
+
