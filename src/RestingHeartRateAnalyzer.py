f68bbb9e0e0a48ec5c59b97883bd25a4a7ee2694
michellejee <mjee@westmont.edu>
2023-12-10 19:43:19 -0800

    resting heart rate update

--- src/RestingHeartRateAnalyzer.py
+++ src/RestingHeartRateAnalyzer.py
@@ -129,7 +129,7 @@ class RestingHeartRateAnalyzer:
 
 
 if __name__ == "__main__":
-    xml_file_path = '/Users/michellejee/PycharmProjects/assignment-5-Information-Retrieval-from-Real-Data-hannah-and-michelle/data/export.xml'
+    xml_file_path = '/Users/michellejee/Desktop/assignment-5-Information-Retrieval-from-Real-Data-hannah-and-michelle/data/export.xml'
     output_file = '/Users/michellejee/PycharmProjects/assignment-5-Information-Retrieval-from-Real-Data-hannah-and-michelle/out/MeeshMonthlyAvg.csv'
     forecast_file = '/Users/michellejee/PycharmProjects/assignment-5-Information-Retrieval-from-Real-Data-hannah-and-michelle/out/MeeshForecast.csv'
 
